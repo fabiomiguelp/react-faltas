1e1e75190fd0dbb44c870a6a0008b0fecebbaf5b
sopascampeao <fabiomanuel@msn.com>
2022-02-14 23:45:17 -0100

    laravel

---
+++ laravel/backend-presencas/app/Models/Presenca.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Presenca extends Model
+{
+    use HasFactory;
+
+    protected $fillable =[
+        'summary',
+        'location',
+        'start',
+        'end',
+        'datainsercao',
+        'chaveunica',
+        'professor',
+        'faltou',
+        'horamarcacao',
+        'nprocesso'
+    ];
+
+}
